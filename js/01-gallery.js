dd1654dfb1ac6c86d859ea76c8efa8e6c3f4a3ac
Nataliia Nazarko <nataliianazarko@gmail.com>
2022-01-23 17:03:35 +0200

    Task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+    
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);   
+    
+galleryContainer.addEventListener('click', onGalleryContainerClick);    
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => 
+                `< div class="gallery__item" >
+                    <a class="gallery__link" href="large-image.jpg">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"
+                        />
+                    </a>
+                </div > `          
+            )
+        .join('');
+}
+
+function onGalleryContainerClick(evt) {
+    const isGalleryItem = evt.target.classList.contains('gallery__item');
+    if (!isGalleryItem) {
+        return;
+    }
+    
+}
